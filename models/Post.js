8dc99f87e0e6976f28f0ea70dc2df80186494a4b
Megan Neely <meganneely@email.arizona.edu>
2020-07-26 19:07:54 -0700

    Working up to and including video 39

---
+++ models/Post.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  // want post to be connected to a user to need reference
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    // name of user - have option to not delete post even if user is deleted
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        // name of user - have option to not delete post even if user is deleted
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
